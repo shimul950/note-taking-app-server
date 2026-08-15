1f1928997193f4b0d9eb3df907e3b0b39e863cc3
Sayedul Islam Shimul <138389460+shimul950@users.noreply.github.com>
2026-08-16 02:33:51 +0600

    Delete dist directory

--- dist/src/app/modules/auth/auth.route.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.authRoutes = void 0;
-const express_1 = require("express");
-const auth_controller_1 = require("./auth.controller");
-const router = (0, express_1.Router)();
-router.post("/register", auth_controller_1.authControllers.register);
-router.post("/login", auth_controller_1.authControllers.login);
-exports.authRoutes = router;
--- dist/src/app/modules/post/post.route.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.postRoutes = void 0;
-const express_1 = require("express");
-const post_controller_1 = require("./post.controller");
-const auth_1 = require("../../../middleware/auth");
-const router = (0, express_1.Router)();
-router.post("/", auth_1.requireAuth, post_controller_1.postControllers.createPost);
-exports.postRoutes = router;
